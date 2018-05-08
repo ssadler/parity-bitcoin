7407bf35c7f182c9825da5e6375f3b08ab5337d8
Travis CI User <travis@example.org>
2018-05-08 14:40:33 +0000

    Update documentation

---
+++ implementors/storage/store/trait.ConfigStore.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["db"] = ["impl&lt;T&gt; ConfigStore for <a class=\"struct\" href=\"db/struct.BlockChainDatabase.html\" title=\"struct db::BlockChainDatabase\">BlockChainDatabase</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"db/kv/trait.KeyValueDatabase.html\" title=\"trait db::kv::KeyValueDatabase\">KeyValueDatabase</a>,&nbsp;</span>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ implementors/storage/transaction_provider/trait.TransactionProvider.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors["db"] = ["impl&lt;T&gt; TransactionProvider for <a class=\"struct\" href=\"db/struct.BlockChainDatabase.html\" title=\"struct db::BlockChainDatabase\">BlockChainDatabase</a>&lt;T&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;T: <a class=\"trait\" href=\"db/kv/trait.KeyValueDatabase.html\" title=\"trait db::kv::KeyValueDatabase\">KeyValueDatabase</a>,&nbsp;</span>",];
+implementors["miner"] = ["impl TransactionProvider for <a class=\"struct\" href=\"miner/struct.MemoryPool.html\" title=\"struct miner::MemoryPool\">MemoryPool</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
